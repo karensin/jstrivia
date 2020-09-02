cc8ea7c4514c417475ea0ca7ae78776c2bdd1f47
Karen k  sin <karenksin@MacBook-Pro.attlocal.net>
2020-09-02 14:31:06 -0700

    Q&A added

--- src/App.js
+++ src/App.js
@@ -22,7 +22,12 @@ import './App.css';
 //when all cards are disabled show message - Congrats, you have completed the game 
 //show button to restart the game 
 //when restart the game, make sure only new questions are rendered to the next game
+
+
+//process- made json file for all categories- each with 20 Q&A
 function App() {
+
+
   return (
     <div className="App">
       <header className="App-header">
